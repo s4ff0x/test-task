a6253e885a8a46639d29f1fd9f14b2d7bf230620
antonm <mendelson.inbox@gmail.com>
2024-06-15 12:24:55 +0300

    feat: configs

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  arrowParens: 'avoid',
+  jsxBracketSameLine: true,
+  printWidth: 80,
+  jsxSingleQuote: false,
+}
